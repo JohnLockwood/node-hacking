044cd2479f521ecd72dac6a5984585b456d8022e
John Lockwood <john.lockwood1@hitachivantara.com>
2021-03-19 16:14:12 -0400

    README and get mocha working with .mjs files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Just Some Node Hacking
+
+... to learn me some node for a great good
+
+* jest_testing hasn't worked out well yet.  Module fun at the heart of it mostly
+* mocha_testing went quite well and was very node-friendly
+* mjs_mocha_testing - Trying to see if we can use [ECMA style imports](https://nodejs.org/api/packages.html#packages_determining_module_system)
---
+++ mjs_mocha_testing/test/test.mjs
@@ -0,0 +1,37 @@
+import sum from '../sum.mjs';
+import assert from 'assert';
+import {expect} from 'chai';
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe('sum', function() {
+    describe('it should sum values correctly()', function() {
+      it('should sum positive values correctly', function() {
+        assert(sum(1,2) == 3);
+      });
+      it('should sum positive + negative values correctly', function() {
+        assert(sum(-1,2) == 1);
+      });
+      it('should sum all negative values correctly', function() {
+        assert(sum(-1,-2) == -3);
+      });
+      it('should sum positive values correctly (chai version)', function() {
+        expect(sum(1,2)).to.equal(3);
+      });
+      it('should sum positive + negative values correctly (chai version)', function() {
+        expect(sum(-1,2)).to.equal(1);
+      });
+      it('should sum all negative values correctly, (chai version)', function() {
+        expect(sum(-1,-8)).to.equal(-9);
+      });
+
+    });
+  });
+
+
